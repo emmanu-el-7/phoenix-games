cbefc2ee5cd75a3a0818b88eacd55c2497a8f095
Emmanuel Rodrigues da Rocha Costa <rodriguesemanuel021@gmail.com>
2024-06-07 15:21:07 -0300

    home finalizada

---
+++ frontend/phoenix-games/src/pages/Categories/Categories.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Categories = ({ products, reference }) => {
+	return (
+		<section className='categories' ref={reference}>
+			<div className='container-categories'>
+                <div className="row">
+                    
+                </div>
+            </div>
+		</section>
+	);
+};
+
+export default Categories;
